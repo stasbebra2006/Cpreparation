630fac94d8a5950210f16744fe31652a80cf8cb3
Stanislav Liashevskyi <liashsta@fit.cvut.cz>
2025-02-11 16:43:54 +0100

    vysilacky1: had to make update earlier, but find, now working on find_distance_between, won't break a lot

--- vysilacky1/solution.c
+++ vysilacky1/solution.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 typedef struct station
 {
@@ -42,6 +43,7 @@ void print_stations_list(station *stationsList)
 {
 	if (!stationsList)
 	{
+		printf("NO stations\n");
 	}
 	int i = 1;
 	while (stationsList)
@@ -55,12 +57,23 @@ void print_stations_list(station *stationsList)
 	}
 }
 
-void find_distance_between()
+void find_distance_between(station * stationsList, int * error)
 {
-	return;
+	if(getchar() != ' ')
+	{
+		*error == 1;
+		return;
+	}
+	printf("%% is called\n");
+	char * name1 = read_name(error);
+	if(*error == 1) return;
+	char * name2 = read_name(error);
+	if(*error == 1) return;
 }
+
 void find_the_nearest()
 {
+	printf("? is called\n");
 	return;
 }
 
@@ -71,16 +84,23 @@ char *read_name(int *error)
 	int position = 0;
 	while (1)
 	{
+		//
+		printf("position: %d\n", position);
+		//
 		int checkScanf = scanf("%c", &symbol);
-		if (checkScanf != 1 || (symbol != '/' && !isdigit(symbol) && !isupper(symbol)) || position > 16)
-		{
-			free(name);
-			*error = 1;
-			return NULL;
-		}
 		if (symbol == ' ')
 		{
 			break;
+		}
+		if (checkScanf != 1 || (symbol != '/' && !isdigit(symbol) && !isupper(symbol)) || position >= 16)
+		{
+			//
+			printf("this error 2\n");
+			printf("checked char is - \"%c\"\n", symbol);
+			//
+			free(name);
+			*error = 1;
+			return NULL;
 		}
 		name[position] = symbol;
 		position++;
@@ -94,6 +114,9 @@ station *read_station(station *stationsList, int *error)
 	char *name = read_name(error);
 	if (*error == 1)
 	{
+		//
+		printf("this error 1\n");
+		//
 		return NULL;
 	}
 	int currentX = 0, currentY = 0;
@@ -107,65 +130,83 @@ station *read_station(station *stationsList, int *error)
 	return stationsList;
 }
 
-void crossroad(station *stationsList, int *error)
+station *crossroad(station *stationsList, int *error)
 {
 	int isListComplited = 0;
 	while (1)
 	{
 		char decisionMaker = getchar();
+		//
+		printf("decisionMaker = %c\n", decisionMaker);
+		//
 		if (decisionMaker == EOF)
 		{
 			if (!stationsList)
 			{
 				*error = 1;
-				return;
+				return stationsList;
 			}
-			return;
+			return stationsList;
 		}
 		if (decisionMaker == '\n')
 		{
 			continue;
 		}
-		ungetc(decisionMaker, stdin);
+		//
+		printf("decisionMaker after ungetc = %c\n", decisionMaker);
+		//
 		if (isupper(decisionMaker) || decisionMaker == '/' || isdigit(decisionMaker))
 		{
+			//
+			printf("im here\n");
+			//
 			if (isListComplited == 0)
 			{
+				//
+				printf("im here 2\n");
+				//
+				ungetc(decisionMaker, stdin);
 				stationsList = read_station(stationsList, error);
 				if (*error == 1)
 				{
-					return;
+					return stationsList;
 				}
 			}
 			else
 			{
 				*error = 1;
-				return;
+				return stationsList;
 			}
 		}
 		else
 		{
 			isListComplited++;
+			//
+			printf("isListComplited has changed, it's \"%d\" now", isListComplited);
+			//
 			switch (decisionMaker)
 			{
 			case '%':
-				find_distance_between();
+				find_distance_between(stationsList, error);
+				if(error == 1) return stationsList;
 				break;
 			case '?':
 				find_the_nearest();
+				break;
 			default:
 				*error = 1;
-				return;
+				return stationsList;
 			}
 		}
 	}
+	return stationsList;
 }
 
 int main()
 {
 	int error = 0;
 	station *stationsList = stations_init();
-	crossroad(stationsList, &error);
+	stationsList = crossroad(stationsList, &error);
 	if (error == 1)
 	{
 		printf("Nespravny vstup.\n");
