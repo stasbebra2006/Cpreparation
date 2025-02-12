5758b686ab7bf24e4287e9490a752f51761670d0
Stanislav Liashevskyi <liashsta@fit.cvut.cz>
2025-02-12 14:50:28 +0100

    vysilacky1: Ja bombil

--- vysilacky1/solution.c
+++ vysilacky1/solution.c
@@ -68,7 +68,7 @@ char *read_name(int *error)
 	while (1)
 	{
 		//
-		printf("position: %d\n", position);
+		// printf("position: %d\n", position);
 		//
 		int checkScanf = scanf("%c", &symbol);
 		if (symbol == ' ' || symbol == '\n' || checkScanf == EOF)
@@ -78,10 +78,10 @@ char *read_name(int *error)
 		if (checkScanf != 1 || (symbol != '/' && !isdigit(symbol) && !isupper(symbol)) || position >= 16)
 		{
 			//
-			printf("this error 2\n");
-			printf("checked char is - \"%c\"\n", symbol);
-			printf("position fale is: %d\n", position);
-			printf("checkScanf = \"%d\"\n", checkScanf);
+			// printf("this error 2\n");
+			// printf("checked char is - \"%c\"\n", symbol);
+			// printf("position fale is: %d\n", position);
+			// printf("checkScanf = \"%d\"\n", checkScanf);
 			//
 			free(name);
 			*error = 1;
@@ -106,7 +106,7 @@ station *read_station(station *stationsList, int *error)
 	if (*error == 1)
 	{
 		//
-		printf("this error 1\n");
+		// printf("this error 1\n");
 		//
 		return NULL;
 	}
@@ -123,7 +123,7 @@ station *read_station(station *stationsList, int *error)
 
 double find_distance(station *station1, station *station2)
 {
-	return sqrt(abs(pow(station2->x - station1->x, 2)) + abs(pow(station2->y - station1->y, 2)));
+	return sqrt(pow(station2->x - station1->x, 2) + pow(station2->y - station1->y, 2));
 }
 
 void find_distance_between(station *stationsList, int *error)
@@ -133,7 +133,9 @@ void find_distance_between(station *stationsList, int *error)
 		*error = 1;
 		return;
 	}
-	printf("%% is called\n");
+	//
+	// printf("%% is called\n");
+	//
 	char *name1 = read_name(error);
 	if (*error == 1)
 	{
@@ -148,8 +150,8 @@ void find_distance_between(station *stationsList, int *error)
 		return;
 	}
 	//
-	printf("first name is %s\n", name1);
-	printf("second name is %s\n", name2);
+	// printf("first name is %s\n", name1);
+	// printf("second name is %s\n", name2);
 	//
 
 	station *station1 = NULL;
@@ -169,7 +171,7 @@ void find_distance_between(station *stationsList, int *error)
 	else
 	{
 		double distance = find_distance(station1, station2);
-		printf("distance between is %.2f\n", distance);
+		printf("Vzdalenost: %.2f metru\n", distance);
 	}
 	free(name1);
 	free(name2);
@@ -177,13 +179,15 @@ void find_distance_between(station *stationsList, int *error)
 
 void find_the_nearest(station *stationsList, int *error)
 {
-	printf("? is called\n");
+	//
+	// printf("? is called\n");
+	//
 	int currentX = 0;
 	int currentY = 0;
-	station *holder = calloc(1, sizeof(station));
+	station *holder = (station *)calloc(1, sizeof(station));
 	holder->x = currentX;
 	holder->y = currentY;
-	station *nearest = NULL;
+	station *nearest = stationsList;
 	int checkScanf = scanf(" %d %d", &currentX, &currentY);
 	if (checkScanf != 2)
 	{
@@ -191,14 +195,14 @@ void find_the_nearest(station *stationsList, int *error)
 		return;
 	}
 	//
-	printf("currentX = %d\n", currentX);
-	printf("currentY = %d\n", currentY);
+	// printf("currentX = %d\n", currentX);
+	// printf("currentY = %d\n", currentY);
 	//
 	double distanceMin = DBL_MAX;
 	while (stationsList)
 	{
 		double distanceCurrent = find_distance(holder, stationsList);
-		if (distanceCurrent < distanceMin || (distanceCurrent == distanceMin && strcmp(stationsList->name,nearest->name) < 0))
+		if (distanceCurrent < distanceMin || (distanceCurrent == distanceMin && strcmp(stationsList->name, nearest->name) <= 0)) //
 		{
 			distanceMin = distanceCurrent;
 			nearest = stationsList;
@@ -206,7 +210,7 @@ void find_the_nearest(station *stationsList, int *error)
 		stationsList = stationsList->next;
 	}
 	printf("Nejblizsi: %s %d %d\n", nearest->name, nearest->x, nearest->y);
-	printf("Vzdalenost: %.2f\n", distanceMin);
+	printf("Vzdalenost: %.2f metru\n", distanceMin);
 	free(holder);
 }
 
@@ -217,7 +221,7 @@ station *crossroad(station *stationsList, int *error)
 	{
 		char decisionMaker = getchar();
 		//
-		printf("decisionMaker = %c\n", decisionMaker);
+		// printf("decisionMaker = %c\n", decisionMaker);
 		//
 		if (decisionMaker == EOF)
 		{
@@ -233,17 +237,17 @@ station *crossroad(station *stationsList, int *error)
 			continue;
 		}
 		//
-		printf("decisionMaker after ungetc = %c\n", decisionMaker);
+		// printf("decisionMaker after ungetc = %c\n", decisionMaker);
 		//
 		if (isupper(decisionMaker) || decisionMaker == '/' || isdigit(decisionMaker))
 		{
 			//
-			printf("im here\n");
+			// printf("im here\n");
 			//
 			if (isListComplited == 0)
 			{
 				//
-				printf("im here 2\n");
+				// printf("im here 2\n");
 				//
 				ungetc(decisionMaker, stdin);
 				stationsList = read_station(stationsList, error);
@@ -262,7 +266,7 @@ station *crossroad(station *stationsList, int *error)
 		{
 			isListComplited++;
 			//
-			printf("isListComplited has changed, it's \"%d\" now\n", isListComplited);
+			// printf("isListComplited has changed, it's \"%d\" now\n", isListComplited);
 			//
 			switch (decisionMaker)
 			{
@@ -287,6 +291,7 @@ station *crossroad(station *stationsList, int *error)
 
 int main()
 {
+	printf("Zadejte stanice a potom dotazy:\n");
 	int error = 0;
 	station *stationsList = stations_init();
 	stationsList = crossroad(stationsList, &error);
@@ -296,6 +301,7 @@ int main()
 		free_stations_list(stationsList);
 		return 1;
 	}
-	print_stations_list(stationsList);
+	// print_stations_list(stationsList);
 	free_stations_list(stationsList);
+	return 0;
 }
